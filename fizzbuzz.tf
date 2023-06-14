ae26bb031b67c0090a13d7b3e9128bcc5de40d5a
Mark J. Reed <markjreed@gmail.com>
2023-06-14 10:49:39 -0400

    fix: formatting

--- fizzbuzz.tf
+++ fizzbuzz.tf
@@ -1,9 +1,9 @@
 output "fizzbuzz" {
   value = <<EOT
     %{~ for i in range(1,101) ~}
-       %{~if i % 3 == 0}Fizz%{ endif ~}
-       %{~if i % 5 == 0}Buzz%{ endif ~}
-       %{~if i % 3 > 0 && i % 5 > 0}${i}%{ endif }
-     %{~ endfor ~}
+      %{~if i % 3 == 0}Fizz%{ endif ~}
+      %{~if i % 5 == 0}Buzz%{ endif ~}
+      %{~if i % 3 > 0 && i % 5 > 0}${i}%{ endif }
+    %{~ endfor ~}
 EOT
 }

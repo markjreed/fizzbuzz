ccaa7d36e20cf93ca0aac859994b47469f82ed0d
Mark J. Reed <markjreed@gmail.com>
2023-06-15 17:27:12 -0400

    feat: fizzbuzz implementation in ><>

---
+++ fizzbuzz.fish
@@ -0,0 +1,8 @@
+1v                               <
+ >:f%?v"FizzBuzz"                 v
+      >:3%?v"Fizz"                v
+           >:5%?v"Buzz"           v
+                >:n>ao1+:55*4*)?;^
+                                  >r&v
+l0=?vo                               >
+    >&             ^

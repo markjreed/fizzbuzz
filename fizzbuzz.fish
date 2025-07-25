5b626242e07babc2e4cbe4721ea99864d724a1bd
Mark J. Reed <markjreed@gmail.com>
2025-07-25 16:24:51 -0400

    feat: Fish shell implementation

--- fizzbuzz.fish
+++ fizzbuzz.fish
@@ -1,8 +1,16 @@
-1v                               <
- >:f%?v"FizzBuzz"                 v
-      >:3%?v"Fizz"                v
-           >:5%?v"Buzz"           v
-                >:n>ao1+:55*4*)?;^
-                                  >r&v
-l0=?vo                               >
-    >&             ^
+#!/usr/bin/env fish
+for i in (seq 100)
+    set print_num true
+    if test (math $i % 3) -eq 0 
+        echo -n Fizz
+        set print_num false
+    end
+    if test (math $i % 5) -eq 0 
+        echo -n Buzz
+        set print_num false
+    end
+    if $print_num 
+        echo -n $i
+    end
+    echo
+end
